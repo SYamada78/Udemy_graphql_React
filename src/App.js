6ab491d57cfaa3b809984c7b951b09efaecc7d79
YamadaShota <yamada_shota@lightcafe.co.jp>
2022-04-30 01:08:52 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Auth from './components/Auth'; 
+import MainPage from './components/MainPage';
+import StateContextProvider from './context/StateContext';
+
+const httpLink = createHttpLink({
+  uri: "http://127.0.0.1:8000/graphql/",
+  credentials: 'include'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <StateContextProvider>
+        <div className={styles.app}>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<Auth />} />
+              <Route path="employees" element={<MainPage />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </StateContextProvider>
+    </ApolloProvider>
   );
 }
 
